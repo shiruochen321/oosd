3b11f8224b3f668d7d36cde5678736264aa945fd
shiruochen321 <shiruochen0801@126.com>
2025-09-11 11:00:06 +0100

    Worked on lab 1

---
+++ Lab1/ThermTest.java
@@ -0,0 +1,8 @@
+public class ThermTest {
+    public static void main(String[] args) {
+        Thermometer thermA = new Thermometer();
+        thermA.setCelsius(20.0);
+        System.out.println("Temp. of Thermoneter A is" + thermA.getCelsius());
+    }
+    
+}
---
+++ Lab1/Thermometer.java
@@ -0,0 +1,15 @@
+
+public class Thermometer {
+  private double celsius;
+
+  public double getCelsius() {
+    return celsius;
+  }
+
+  public void setCelsius(double celsius) {
+    this.celsius = celsius;
+  }
+  
+}
+    
+
